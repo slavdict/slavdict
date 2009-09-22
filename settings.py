cbe603b9680c114c01a70a9e0d11812486002b9b
Andrej Khitrov <a.n.khitrov@gmail.com>
2009-09-22 18:06:56 +0400

    added code template files and settings changed

--- settings.py
+++ settings.py
@@ -1,10 +1,12 @@
 # Django settings for cslav_dict project.
 
-DEBUG = True
+ROOT = '/home/hitrov/djcode/cslav_dict'
+
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    ('Andrej Khitrov', 'a.n.khitrov@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -21,13 +23,13 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Etc/GMT+3'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
-SITE_ID = 1
+SITE_ID = 3
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
@@ -35,7 +37,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = ROOT + '/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
