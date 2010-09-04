4a8eb76fa5a025f31f7d4c03f73c4daea0e5a7c0
while0pass <while0pass@yandex.ru>
2010-09-05 02:08:56 +0400

    Поле "Притяж. прил." (FK) разбито на два.
    Поле "Притяжательное прилагательное" (FK) разбито на поле
    "прилагательное от сущ." (FK) и "притяжательное прилагательное" (Bool).
    Кроме того удалено лишнее поле "Притяж. прил." (FK), возникшее из-за
    недосмотра.

--- dictionary/models.py
+++ dictionary/models.py
@@ -143,22 +143,20 @@ class Entry(models.Model, AdminInfo):
         blank = True,
         )
 
-    possessive_pronoun_to = models.ForeignKey(
-        'self',
-        verbose_name = u'притяж. прил. к',
-        blank = True,
-        null = True,
-        )
-
     @property
     def short_form_ucs(self):
         return ucs_convert(self.short_form)
 
-    possessive_prounoun_to = models.ForeignKey(
+    noun_derived_adjective = models.ForeignKey(
         'self',
-        verbose_name = u'притяж. прилагательное от',
+        verbose_name = u'прилаг., образованное от сущ.',
         blank = True,
         null = True,
+        )
+
+    possessive = models.BooleanField(
+        u'притяжательное прилагательное',
+        default = False,
         )
 
     # только для глаголов
