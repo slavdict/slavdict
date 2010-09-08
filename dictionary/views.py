55114584b938f26f5c3ce43204089d3465844cf4
Andrej Khitrov <a.n.khitrov@gmail.com>
2010-09-08 20:51:51 +0400

    Написана первая view function. URLconf переделан с её учётом.

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from slavdict.dictionary.models import Entry
+
+def all_entries(request):
+
+    entries = Entry.objects.all()
+
+    return render_to_response(
+
+                'template1.django.html',
+
+                { 'entries': entries, },
+
+                RequestContext(request),
+
+                )
--- urls.py
+++ urls.py
@@ -7,25 +7,13 @@ from django.shortcuts import redirect
 from django.contrib import admin
 admin.autodiscover()
 
-from slavdict.dictionary.models import Entry
-entries = Entry.objects.all()
-
-test = {
-    'template': 'template1.django.html',
-    'extra_context': {
-            'title': u'Словарные статьи',
-            'content': u'Тестовое содержимое страницы. Ура, сайт работает!',
-            'entries': entries,
-        }
-    }
-
 urlpatterns = patterns('',
     url( r'^admin/',     include(admin.site.urls)),
     url( r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
 
 urlpatterns += patterns('',
-    url( r'^$',      direct_to_template,     test),
+    url( r'^$',      'dictionary.all_entries'),
     url( r'^forum/', include('slavdict.forum.urls')),
     url( r'^wiki/$', redirect('http://slavonic.pbworks.com/')),
 )
