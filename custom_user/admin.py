88dd197a01b307253c40db2640af03d28928f8b0
while0pass <while0pass@yandex.ru>
2010-09-12 04:09:34 +0400

    Устранены проблемы с отображением новых полей CustomUser в админке.

--- custom_user/admin.py
+++ custom_user/admin.py
@@ -2,46 +2,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-
 from slavdict.custom_user.models import CustomUser
+from slavdict.custom_user.forms import CustomUserChangeForm, CustomUserCreationForm
 
 class CustomUserAdmin(UserAdmin):
+
     list_display = ('username', '__unicode__',
                     'is_staff', 'is_active')
-    fieldsets = (
-        (None, {
-            'fields': (
-                'username',
-                'password',
-                ),
-            }),
-        (u'персональная инормация', {
-            'fields': (
-                # second_name -- добавленное в CustomUser поле.
-                ('last_name', 'first_name', 'second_name'),
-                'email',
-                ),
-            }),
-        (u'права', {
-            'fields': (
-                'is_active',
-                'is_staff',
-                'is_superuser',
-                'user_permissions',
-                ),
-            }),
-        (u'важные даты', {
-            'fields': (
-                'last_login',
-                'date_joined',
-                ),
-            }),
-        (u'группы', {
-            'fields': (
-                'groups',
-                ),
-            }),
+
+    # Поле fildsets делаем как у UserAdmin.
+    # Меняем там блок полей, озаглавленный Personal Info.
+    # См. django.contrib.auth.admin.UserAdmin
+    fieldsets = UserAdmin.fieldsets
+    fieldsets[1][1]['fields'] = (
+        ('last_name', 'first_name', 'second_name'),
+        'email',
         )
+
+    # В полях форм используем наши формы,
+    # где используется модель CustomUser вместо User.
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
 
 admin.site.unregister(User)
 admin.site.register(CustomUser, CustomUserAdmin)
---
+++ custom_user/forms.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Создаём производные от форм UserChangeForm и UserCreationForm
+из django.contrib.auth.forms. Переопределяем в них все методы
+и классы, где используется модель User, заменяя её на CustomUser.
+
+Если не создать таких производных, админка будет жаловаться,
+что в формах нет новых полей, определённых в CustomUser.
+
+"""
+from django import forms
+from slavdict.custom_user.models import CustomUser
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.utils.translation import ugettext_lazy as _
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ("username",)
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        try:
+            CustomUser.objects.get(username=username)
+        except CustomUser.DoesNotExist:
+            return username
+        raise forms.ValidationError(_("A user with that username already exists."))
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
