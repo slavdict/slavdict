8c95ae03541410814338b5ecd31165d9a6d6223b
while0pass <while0pass@yandex.ru>
2010-10-18 08:04:23 +0400

    temp

--- directory/admin.py
+++ directory/admin.py
@@ -1,29 +1,6 @@
 # -*- coding: UTF-8 -*-
 from django.conf import settings
 from django.contrib import admin
-from slavdict.directory.models import (
-
-    PartOfSpeech,
-    Gender,
-    Tantum,
-    Onym,
-    Transitivity,
-    SyntArgument,
-    SubcatFrame,
-    Language,
-    EntryStatus,
-
-)
-
-admin.site.register(PartOfSpeech)
-admin.site.register(Gender)
-admin.site.register(Tantum)
-admin.site.register(Onym)
-admin.site.register(Transitivity)
-admin.site.register(SyntArgument)
-admin.site.register(SubcatFrame)
-admin.site.register(Language)
-admin.site.register(EntryStatus)
 
 from slavdict.directory.models import ValueTag
 class ValueTag_Inline(admin.StackedInline):
--- directory/models.py
+++ directory/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
         help_text = u'''Для использования при формировании
                         ручных запросов к БД, вроде «Выдай
                         мне все значения категории slug».''',
-        max_length = 15,
+        max_length = 50,
         )
 
     description = models.CharField(
@@ -18,7 +18,7 @@ class Category(models.Model):
 
     tag = models.CharField(
         u'ярлык по умолчанию',
-        max_length = 15,
+        max_length = 50,
         )
 
     def __unicode__(self):
@@ -41,12 +41,12 @@ class CategoryValue(models.Model):
         help_text = u'''Для использования при формировании
                         ручных запросов к БД, вроде «Выдай
                         мне все возможные ярлыки для значения slug».''',
-        max_length = 15,
+        max_length = 50,
         )
 
     tag = models.CharField(
         u'ярлык по умолчанию',
-        max_length = 15,
+        max_length = 50,
         )
 
     order = models.PositiveSmallIntegerField(
@@ -69,6 +69,25 @@ class CategoryValue(models.Model):
         max_length = 50,
         )
 
+    # Данное поле первоначально придумано для указания CSS-класса,
+    # потребовавшагося для отображении надписей на разных языках с
+    # использованием разных шрифтов.
+    css_class = models.CharField(
+        u'CSS-класс',
+        max_length = 50,
+        blank = True,
+        null = True,
+        )
+
+    # Данное поле первоначально придумано для указания 2-го CSS-класса,
+    # потребовавшагося для отображении транслита некоторых языков.
+    css_class2 = models.CharField(
+        u'CSS-класс 2',
+        max_length = 50,
+        blank = True,
+        null = True,
+        )
+
     def __unicode__(self):
         return self.tag
 
@@ -85,7 +104,7 @@ class TagLibrary(models.Model):
         help_text = u'''Для использования при формировании
                         ручных запросов к БД, вроде «Выдай
                         мне все возможные ярлыки из библиотеки slug».''',
-        max_length = 15,
+        max_length = 50,
         )
 
     description = models.TextField(
@@ -142,7 +161,7 @@ class ValueTag(models.Model):
 
     tag = models.CharField(
         u'ярлык',
-        max_length = 20,
+        max_length = 50,
         )
 
     # Для возможности поменять порядок следования значений категории.
@@ -156,11 +175,32 @@ class ValueTag(models.Model):
     pinned = models.BooleanField(
         u'закрепить',
         help_text = u'''Для возможности разбить все ярлыки в данной категории
-                        значений на часто используемые, которые необходимо выдать
-                        группой в начале, и редкоиспользуемые, чтобы сделать, например,
-                        для них ссылку «другое», при нажатии на которую уже и будут
-                        они появляться для возможности выбора.''',
+                        значений на часто используемые, которые необходимо
+                        выдать группой в начале, и редкоиспользуемые, чтобы
+                        сделать, например, для них ссылку «другое», при нажатии
+                        на которую уже и будут они появляться для возможности
+                        выбора.''',
         )
+
+    # Данное поле первоначально придумано для указания CSS-класса,
+    # потребовавшагося для отображении надписей на разных языках с
+    # использованием разных шрифтов.
+    css_class = models.CharField(
+        u'CSS-класс',
+        max_length = 50,
+        blank = True,
+        null = True,
+        )
+
+    # Данное поле первоначально придумано для указания 2-го CSS-класса,
+    # потребовавшагося для отображении транслита некоторых языков.
+    css_class2 = models.CharField(
+        u'CSS-класс 2',
+        max_length = 50,
+        blank = True,
+        null = True,
+        )
+
 
     def __unicode__(self):
         return self.tag
@@ -169,163 +209,3 @@ class ValueTag(models.Model):
         verbose_name = u'ярлык для значения'
         verbose_name_plural = u'ярлыки для значений'
         ordering = ('taglib', 'catvalue', 'catvalue__order')
-
-
-################################################
-
-class TermDirectory(models.Model):
-
-    name = models.CharField(
-        u'название',
-        max_length=20,
-        )
-
-    abbreviation = models.CharField(
-        u'сокращение',
-        max_length=8,
-        )
-
-    def __unicode__(self):
-        return u'%s [%s]' % (self.name, self.abbreviation)
-
-    class Meta:
-        abstract = True
-        ordering = ('name',)
-
-
-class PartOfSpeech(TermDirectory):
-
-    def __unicode__(self):
-        return self.abbreviation
-
-    class Meta(TermDirectory.Meta):
-        verbose_name = u'часть речи'
-        verbose_name_plural = u'части речи'
-
-
-class Gender(TermDirectory):
-
-    class Meta(TermDirectory.Meta):
-        verbose_name = u'грамматический род'
-        verbose_name_plural = u'значения грамматического рода'
-
-
-class Tantum(TermDirectory):
-
-    class Meta(TermDirectory.Meta):
-        verbose_name = u'tantum'
-        verbose_name_plural = u'tantum'
-
-
-class Onym(TermDirectory):
-
-    class Meta(TermDirectory.Meta):
-        verbose_name = u'тип имени собственного'
-        verbose_name_plural = u'типы имени собственного'
-
-
-class Transitivity(TermDirectory):
-
-    class Meta(TermDirectory.Meta):
-        verbose_name = u'переходность глагола'
-        verbose_name_plural = u'переходность глагола'
-
-class SyntArgument(TermDirectory):
-
-    class Meta(TermDirectory.Meta):
-        verbose_name = u'синтаксический аргумент'
-        verbose_name_plural = u'синтаксические аргументы'
-
-
-class SubcatFrame(models.Model):
-
-    argument1 = models.ForeignKey(
-        SyntArgument,
-        verbose_name = u'1-й аргумент',
-        related_name = 'argument1_set',
-        )
-
-    argument2 = models.ForeignKey(
-        SyntArgument,
-        verbose_name = u'2-й аргумент',
-        related_name = 'argument2_set',
-        blank = True,
-        null = True,
-        )
-
-    argument3 = models.ForeignKey(
-        SyntArgument,
-        verbose_name = u'3-й аргумент',
-        related_name = 'argument3_set',
-        blank = True,
-        null = True,
-        )
-
-    argument4 = models.ForeignKey(
-        SyntArgument,
-        verbose_name = u'4-й аргумент',
-        related_name = 'argument4_set',
-        blank = True,
-        null = True,
-        )
-
-    def __unicode__(self):
-
-        rtn = self.argument1.abbreviation
-        if self.argument2:
-            rtn = u'%s, %s' % (rtn, self.argument2.abbreviation)
-        if self.argument3:
-            rtn = u'%s, %s' % (rtn, self.argument3.abbreviation)
-        if self.argument4:
-            rtn = u'%s, %s' % (rtn, self.argument4.abbreviation)
-        return rtn
-
-    class Meta:
-        verbose_name = u'модель управления'
-        verbose_name_plural = u'модели управления'
-
-
-class Language(TermDirectory):
-
-    css_class = models.CharField(
-        u'CSS-класс',
-        help_text = u'''Укажите CSS-класс, который необходимо
-                        использовать при отображении надписей
-                        на данном языке.''',
-        max_length = 30,
-        blank = True,
-        null = True,
-        )
-
-    translit_css_class = models.CharField(
-        u'CSS-класс транслита',
-        help_text = u'''Укажите CSS-класс, который необходимо
-                        использовать при отображении транслитераций
-                        с данного языка.''',
-        max_length = 30,
-        blank = True,
-        null = True,
-        )
-
-    class Meta(TermDirectory.Meta):
-        verbose_name = u'язык'
-        verbose_name_plural = u'список языков'
-
-class EntryStatus(models.Model):
-
-    order = models.IntegerField(
-        u'порядковый номер',
-        )
-
-    status = models.CharField(
-        u'статус',
-        max_length = 30,
-        )
-
-    def __unicode__(self):
-        return self.status
-
-    class Meta:
-        verbose_name = u'статус словарной статьи'
-        verbose_name_plural = u'статусы словарной статьи'
-        ordering = ('order',)
