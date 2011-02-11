9fe30182d5e10cb0bfe26bdd42f9e3b0f71ba54c
while0pass <while0pass@yandex.ru>
2011-02-12 00:09:35 +0300

    Добавлен набор форм OrthVar

--- dictionary/views.py
+++ dictionary/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render_to_response
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.template import RequestContext
-from slavdict.dictionary.models import Entry, Meaning, Example
+from slavdict.dictionary.models import Entry, \
+    Meaning, Example, OrthographicVariant, Etymology
 import datetime
 
 def all_entries(request):
@@ -58,6 +59,9 @@ def change_entry(request, entry_id):
 
     entry = Entry.objects.get(pk=entry_id)
 
+    # Орфографические варианты.
+    orth_vars = entry.orth_vars
+
     # Значения данной словарной статьи.
     meanings = Meaning.objects.filter(entry_container=entry)
     l = range(len(meanings))
@@ -68,20 +72,24 @@ def change_entry(request, entry_id):
 
     MeaningFormSet = modelformset_factory(Meaning)
     ExampleFormSet = modelformset_factory(Example)
+    OrthVarFormSet = modelformset_factory(OrthographicVariant)
 
     if request.method == "POST":
         entry_form = EntryForm(request.POST, instance=entry)
         meaning_formset = MeaningFormSet(request.POST,
             queryset=meanings, prefix='meaning')
         example_formset_groups = [ExampleFormSet(request.POST, queryset=eg) for eg in example_groups]
+        orth_var_formset = OrthVarFormSet(request.POST, queryset=orth_vars, prefix='orthvar')
     else:
         entry_form = EntryForm(instance=entry)
         meaning_formset = MeaningFormSet(queryset=meanings, prefix='meaning')
         example_formset_groups = [ExampleFormSet(queryset=eg) for eg in example_groups]
+        orth_var_formset = OrthVarFormSet(queryset=orth_vars, prefix='orthvar')
 
     meaning_formset.with_examples = [(meaning_formset.forms[i], example_formset_groups[i]) for i in l]
 
     return render_to_response("change_form.html", {
         'entry_form': entry_form,
         'meaning_formset': meaning_formset,
+        'orth_var_formset': orth_var_formset,
     })
