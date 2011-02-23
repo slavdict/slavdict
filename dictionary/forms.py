dca4517d5facad2e8206c859b09e84bccef48c78
while0pass <while0pass@yandex.ru>
2011-02-23 23:37:15 +0300

    Многие поля в формах сделаны скрытыми.
    Заметно увеличилась производительности при отрисовке форм, т.к. поля
    select с большим числом option'ов (порядка 200 и более) заменены на hidden.

--- dictionary/forms.py
+++ dictionary/forms.py
@@ -9,6 +9,28 @@ from dictionary.models import Entry, Meaning, Example, \
 class EntryForm(ModelForm):
     class Meta:
         model = Entry
+        widgets = {
+            'civil_equivalent': HiddenInput,
+            'hidden': HiddenInput,
+            'homonym_order': HiddenInput,
+            'homonym_gloss': HiddenInput,
+            'word_forms_list': HiddenInput,
+            'transitivity': HiddenInput,
+
+            'derivation_entry': HiddenInput,
+            'link_to_entry': HiddenInput,
+            'link_to_collogroup': HiddenInput,
+            'link_to_meaning': HiddenInput,
+            'cf_entries': HiddenInput,
+            'cf_collogroups': HiddenInput,
+            'cf_meanings': HiddenInput,
+
+            'status': HiddenInput,
+            'percent_status': HiddenInput,
+            'editor': HiddenInput,
+            'antconc_query': HiddenInput,
+            'grequiv_status': HiddenInput,
+        }
 
 class MeaningForm(ModelForm):
 
@@ -18,6 +40,15 @@ class MeaningForm(ModelForm):
             'entry_container': HiddenInput,
             'collogroup_container': HiddenInput,
             'order': HiddenInput,
+            'parent_meaning': HiddenInput,
+            'hidden': HiddenInput,
+
+            'link_to_meaning': HiddenInput,
+            'link_to_entry': HiddenInput,
+            'link_to_collogroup': HiddenInput,
+            'cf_entries': HiddenInput,
+            'cf_collogroups': HiddenInput,
+            'cf_meanings': HiddenInput,
         }
 
 class ExampleForm(ModelForm):
@@ -26,6 +57,7 @@ class ExampleForm(ModelForm):
         widgets = {
             'meaning': HiddenInput,
             'order': HiddenInput,
+            'hidden': HiddenInput,
         }
 
 class OrthVarForm(ModelForm):
@@ -43,6 +75,7 @@ class EtymologyForm(ModelForm):
             'entry': HiddenInput,
             'collocation': HiddenInput,
             'order': HiddenInput,
+            'etymon_to': HiddenInput,
         }
 
 class MnngCntxtForm(ModelForm):
@@ -65,6 +98,7 @@ class GrEqForExForm(ModelForm):
         model = GreekEquivalentForExample
         widgets = {
             'for_example': HiddenInput,
+            'position': HiddenInput,
         }
 
 
