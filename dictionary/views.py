af5d2ad43642b8bc13535c643834a3fdff08b267
Andrej Khitrov <a.n.khitrov@gmail.com>
2011-07-02 21:06:59 +0400

    Функции создания PDF вынесены в отдельный модуль.

--- dictionary/views.py
+++ dictionary/views.py
@@ -529,33 +529,8 @@ def import_csv_billet(request):
     return render_to_response('csv_import.html', {'form': form})
 
 
-from django import http
-from django.template.loader import render_to_string
-import ho.pisa as pisa
-import cStringIO as StringIO
-from django.conf import settings
 
-def write_pdf(request, template_src, context_dict):
-    html  = render_to_string(template_src, context_dict, RequestContext(request))
-    result = StringIO.StringIO()
-    pdf = pisa.pisaDocument(
-        StringIO.StringIO(html.encode('UTF-8')),
-        result,
-        path=settings.STATIC_ROOT + 'stub')
-        # Аргумент path внутренне используется следующим образом.
-        # pisaDocument преполагает, что это тот путь, по которому должен был
-        # быть расположен наш html-файл. На основе этого пути вычисляется
-        # путь объемлющей директории. И он принимается за отправную точку
-        # для вычисления относительных ссылок в файле. В данном случае
-        # строчка 'stub' в `path = settings.STATIC_ROOT + 'stub'` используется
-        # только для того, чтобы она была отсечена и чтобы в качестве отправной
-        # директории использовался путь settings.STATIC_ROOT.
-    if not pdf.err:
-        response = http.HttpResponse(result.getvalue(), mimetype='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename=%s' % context_dict['filename']
-        return response
-    return http.HttpResponseRedirect('/')
-
+from slavdict.pdf import write_pdf
 
 @login_required
 def pdf_for_single_entry(request, entry_id):
