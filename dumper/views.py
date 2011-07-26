ae362e1df5526296e6eabeb6487c73487219aa51
while0pass <while0pass@yandex.ru>
2011-07-26 21:09:17 +0400

    Обновление инфо о посл.времени сохранения любой статьи с пом. сигналов

--- dumper/views.py
+++ dumper/views.py
@@ -5,7 +5,7 @@ from django.core.management import call_command
 from django.http import HttpResponse
 
 from dumper.models import Dump
-from dictionry.models import Entry
+from dictionary.models import Entry
 import dumper.gauge as Gauge
 
 default_datetime = datetime.datetime(year=1961, month=4, day=12, hour=9, minute=7)
@@ -20,6 +20,16 @@ try:
 except Entry.DoesNotExist:
     Gauge.LATEST_CHANGE['dictionary'] = default_datetime
 #output = call_command('dumpdata', 'dictionary', format='xml', indent=4)
+
+
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+@receiver(post_save, sender=Entry, dispatch_uid="dict_save_monitor")
+def dict_save_monitor(sender, **kwargs):
+    Gauge.LATEST_CHANGE['dictionary'] = datetime.datetime.now()
+    print datetime.datetime.strftime(Gauge.LATEST_CHANGE['dictionary'], format='%Y.%m.%d %H:%M:%S')
+
 
 @login_required
 @user_passes_test(lambda u: u.is_superuser)
