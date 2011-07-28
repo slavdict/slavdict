86ae9348a952c16e774468d1dd02bacaad6c6c93
while0pass <while0pass@yandex.ru>
2011-07-28 21:30:18 +0400

    Скрипт создания дампа удаляет файл, если он ничем не отл. от последнего

--- dump.sh
+++ dump.sh
@@ -3,5 +3,16 @@ NOW=$(date +"%Y.%m.%d--%H.%M")
 DBS_VERSION=14
 PRJDIR="/var/www/slavdict"
 DUMPDIR="$PRJDIR/.dumps"
+LASTFILE=$(ls -t $DUMPDIR | head -1)
 FILE=".dictionary--$NOW---$DBS_VERSION.xml"
+
 python $PRJDIR/manage.py dumpdata dictionary --format=xml --indent=4 > $DUMPDIR/$FILE
+
+if [ LASTFILE ]
+then
+    x=$(diff $DUMPDIR/$FILE $DUMPDIR/$LASTFILE)
+    if [ -z $x ]
+    then rm $DUMPDIR/$FILE
+    fi
+fi
+
