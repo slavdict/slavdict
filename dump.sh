18c56ba74456a2606a56b9a923ed3eae973ce012
while0pass <while0pass@yandex.ru>
2011-08-04 18:01:01 +0400

    Отслеживание разницы в сжатых архивах напрямую не получится.

--- dump.sh
+++ dump.sh
@@ -3,19 +3,22 @@ NOW=$(date +"%Y.%m.%d--%H.%M")
 DBS_VERSION=16
 PRJDIR="$( cd "$( dirname "$0" )" && pwd )" # ... dirname "$( readlink -f "$0" )" ...
 DUMPDIR="$PRJDIR/.dumps"
-LASTFILE=$(ls -tA $DUMPDIR | head -1)
+LASTFILE=$(ls -tA $DUMPDIR/.dictionary*.xml | head -1)
 FILE=".dictionary--$NOW---$DBS_VERSION.xml"
 
 python $PRJDIR/manage.py dumpdata dictionary --format=xml --indent=4 > $DUMPDIR/$FILE
 
-gzip $DUMPDIR/$FILE
-FILE="$FILE.gz"
-
 if [ "$LASTFILE" -a "$FILE" != "$LASTFILE" ]
 then
     x=$(diff $DUMPDIR/$FILE $DUMPDIR/$LASTFILE)
+
     if [ -z "$x" ]
     then rm $DUMPDIR/$FILE
+    else
+        if [ -a $DUMPDIR/$LASTFILE.gz ]
+        then rm $DUMPDIR/$LASTFILE
+        fi
     fi
 fi
 
+gzip -c $DUMPDIR/$FILE > $DUMPDIR/$FILE.gz
