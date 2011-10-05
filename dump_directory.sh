ece99dce8459d5af2ceea742f6057fb82f7134dc
Andrej Khitrov <a.n.khitrov@gmail.com>
2011-10-06 01:42:50 +0400

    Добавлен скрипт для дампа справочника

---
+++ dump_directory.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+NOW=$(date +"%Y.%m.%d")
+PRJDIR="$( cd "$( dirname "$0" )" && pwd )" # ... dirname "$( readlink -f "$0" )" ...
+DUMPDIR="$PRJDIR/.dumps"
+LASTFILE=$(ls -tA $DUMPDIR/.directory*.xml | head -1)
+FILE="$DUMPDIR/.directory--$NOW.xml"
+
+python $PRJDIR/manage.py dumpdata directory --format=xml --indent=4 > $FILE
+
+if [ "$LASTFILE" -a "$FILE" != "$LASTFILE" ]
+then
+    x=$(diff $FILE $LASTFILE)
+
+    if [ -z "$x" ]
+    then rm $FILE
+    fi
+fi
