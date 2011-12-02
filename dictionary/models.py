77a350fc71234c5d7d82ff4a1df1569799cf48b4
Andrej Khitrov <a.n.khitrov@gmail.com>
2011-12-03 00:53:37 +0400

    Удалены более ненужные статусы греч.параллелей

--- dictionary/models.py
+++ dictionary/models.py
@@ -1084,11 +1084,9 @@ class Example(models.Model):
     GREEK_EQ_STATUS = (
         (u'L', u'следует найти'),   # look for
         (u'S', u'не нужны'),        # stop
-        (u'A', u'проверить'),       # audit
         (u'C', u'уточнить адрес'),  # check the address
         (u'N', u'найти не удалось'),# not found
         (u'F', u'найдены'),         # found
-        (u'x', u'...'),             # временный технический промежуточный статус
         )
 
     greek_eq_status = models.CharField(
