d5f55d295c725cc97f077f5530badb36af0acb08
while0pass <while0pass@yandex.ru>
2012-09-04 12:42:47 +0400

    refactoring...

--- dictionary/utils.py
+++ dictionary/utils.py
@@ -9,32 +9,6 @@ from .models import Etymology
 from .models import GreekEquivalentForExample
 from .models import GreekEquivalentForMeaning
 
-def non_unicode_greek(request):
-    corrupted = 'corrupted' in request.GET
-    nowrap = 'nowrap' in request.GET
-
-    greek_etymons = Etymology.objects.filter(language__slug='greek', corrupted=corrupted)
-    greqex = GreekEquivalentForExample.objects.filter(corrupted=corrupted)
-    greqm = GreekEquivalentForMeaning.objects.filter(corrupted=corrupted)
-
-    words = [(i.text, i.host_entry.id, False, False) for i in greek_etymons if i.text]
-    words.extend([(i.text, i.host_entry.id, False, i.for_example.id) for i in greqex if i.text])
-    words.extend([(i.text, i.host_entry.id, i.for_meaning.id, False) for i in greqm if i.text])
-
-    chardict = defaultdict(set)
-    for word, entry_id, meaning_id, example_id in words:
-        for char in word:
-            chardict[char].add((word, entry_id, meaning_id, example_id))
-    chars = sorted(chardict.keys())
-    context = {
-        'title': u'Неюникодные греческие символы',
-        'chars': chars,
-        'words': chardict,
-        'nowrap': nowrap,
-    }
-    return render_to_response('non_unicode_greek.html', context, RequestContext(request))
-
-
 ULYSSESMAP = (
     (u'\u2026', u'...'),
     (u'\uf020', u' '),
@@ -165,3 +139,30 @@ def ulysses2unicode(text, mapping=ULYSSESMAP):
     for src, dst in mapping:
         text = text.replace(src, dst)
     return text
+
+
+
+def non_unicode_greek(request):
+    corrupted = 'corrupted' in request.GET
+    nowrap = 'nowrap' in request.GET
+
+    greek_etymons = Etymology.objects.filter(language__slug='greek', corrupted=corrupted)
+    greqex = GreekEquivalentForExample.objects.filter(corrupted=corrupted)
+    greqm = GreekEquivalentForMeaning.objects.filter(corrupted=corrupted)
+
+    words = [(i.text, i.host_entry.id, False, False) for i in greek_etymons if i.text]
+    words.extend([(i.text, i.host_entry.id, False, i.for_example.id) for i in greqex if i.text])
+    words.extend([(i.text, i.host_entry.id, i.for_meaning.id, False) for i in greqm if i.text])
+
+    chardict = defaultdict(set)
+    for word, entry_id, meaning_id, example_id in words:
+        for char in word:
+            chardict[char].add((word, entry_id, meaning_id, example_id))
+    chars = sorted(chardict.keys())
+    context = {
+        'title': u'Неюникодные греческие символы',
+        'chars': chars,
+        'words': chardict,
+        'nowrap': nowrap,
+    }
+    return render_to_response('non_unicode_greek.html', context, RequestContext(request))
