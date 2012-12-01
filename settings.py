979017c120fde068e96a50a76ab16a9c297aa8bf
while0pass <while0pass@yandex.ru>
2012-12-01 05:49:27 +0400

    Добавлена мидлваря, отслеживающая версии кук

--- settings.py
+++ settings.py
@@ -95,6 +95,7 @@ JINJA2_EXTENSIONS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'slavdict.middleware.CookieVersionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
