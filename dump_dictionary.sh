55abd0e76f413e73ba8b6c33269ea9aa8a37eed7
while0pass <while0pass@yandex.ru>
2013-05-08 07:46:26 +0400

    Версия бд проставлена в соответсвии с миграциями.

--- dump_dictionary.sh
+++ dump_dictionary.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NOW=$(date +"%Y.%m.%d--%H.%M")
-DBS_VERSION=2.32
+DBS_VERSION=2.34w33
 PRJDIR="$( cd "$( dirname "$0" )" && pwd )" # ... dirname "$( readlink -f "$0" )" ...
 DUMPDIR="$PRJDIR/.dumps"
 LASTFILE=$(ls -tA $DUMPDIR/.dictionary*.xml | head -1)
