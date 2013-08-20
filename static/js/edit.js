887b27bca6e396cb31f5f8bbfc51a171e0413581
while0pass <while0pass@yandex.ru>
2013-08-20 17:54:36 +0400

    Добавлен конструктор-реставратор Etymology.

--- static/js/edit.js
+++ static/js/edit.js
@@ -16,3 +16,30 @@ function upsert(object, attrname, data, defaultValue, observable) {
         object[attrname] = observable(value);
     }
 }
+
+// Конструкторы-реставраторы
+function Etymology(entry, collocation, etymonTo, data) {
+    upsert(this, 'additional_info', data, '');
+    upsert(this, 'collocation_id', data, collocation.id());
+    upsert(this, 'corrupted', data, false);
+    upsert(this, 'entry_id', data, entry.id());
+    upsert(this, 'etymon_to_id', data, etymonTo.id());
+    upsert(this, 'gloss', data, '');
+    upsert(this, 'id', data, 'etymon' + Etymology.counter);
+    upsert(this, 'language', data, 'a' /* греческий */);
+    upsert(this, 'mark', data, '');
+    upsert(this, 'meaning', data, '');
+    upsert(this, 'order', data, 345);
+    upsert(this, 'questionable', data, false);
+    upsert(this, 'source', data, '');
+    upsert(this, 'text', data, '');
+    upsert(this, 'translit', data, '');
+    upsert(this, 'unclear', data, false);
+    upsert(this, 'unitext', data, '');
+    if (!Etymology.idMap.hasOwnProperty(this.id())) {
+        Etymology.counter++;
+        Etymology.idMap[this.id()] = this;
+    }
+}
+Etymology.counter = 0;
+Etymology.idMap = {};
