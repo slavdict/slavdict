32250c0e24a6277136ea3fb523e9207bec9c350f
while0pass <while0pass@yandex.ru>
2013-08-20 17:52:42 +0400

    Добавлена ф-ция добавления объекту наблюдаемого свойства...
    или обновления этого свойства, если оно уже существует.

--- static/js/edit.js
+++ static/js/edit.js
@@ -1,3 +1,18 @@
 function snapshotObservable(value) {
     return ko.observable(value).publishOn('entry change');
 }
+
+function upsert(object, attrname, data, defaultValue, observable) {
+    // Upsert property ``attrname`` in the ``object``
+    var value = data && data[attrname] || defaultValue;
+    observable = observable || snapshotObservable;
+    if (typeof object[attrname] !== 'undefined') {
+        if (ko.isSubscribable(object[attrname])) {
+            object[attrname](value);
+        } else {
+            object[attrname] = value;
+        }
+    } else {
+        object[attrname] = observable(value);
+    }
+}
