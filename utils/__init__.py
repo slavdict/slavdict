f150b2b13734c508f21689408582b794a056c6e5
Nurono <while0pass@yandex.ru>
2014-02-10 20:14:55 +0400

    Возможность править адреса примеров с ипользованием регулярных выражений.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 __all__ = ('p', 's')
 
+import re
+
 from slavdict.dictionary.models import Example
 
 def p(x):
     for e in Example.objects.all():
-        if x in e.address_text:
+        if re.search(x, e.address_text):
             print e.address_text
 
 def s(x, y, without_mtime=True):
     for e in Example.objects.all():
-        if x in e.address_text:
-            e.address_text = e.address_text.replace(x, y)
+        if re.search(x, e.address_text):
+            e.address_text = re.sub(x, y, e.address_text)
             e.save(without_mtime=without_mtime)
