889be112a4b47c28ecaecca3c4ee3913e8c95600
Nurono <while0pass@yandex.ru>
2014-02-10 20:16:00 +0400

    Отображать кол-во найденных/исправленных адресов примеров.

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,12 +6,18 @@ import re
 from slavdict.dictionary.models import Example
 
 def p(x):
+    counter = 0
     for e in Example.objects.all():
         if re.search(x, e.address_text):
             print e.address_text
+            counter += 1
+    print u'Найдено адресов: %i' % counter
 
 def s(x, y, without_mtime=True):
+    counter = 0
     for e in Example.objects.all():
         if re.search(x, e.address_text):
             e.address_text = re.sub(x, y, e.address_text)
             e.save(without_mtime=without_mtime)
+            counter += 1
+    print u'Исправлено адресов: %i' % counter
