4106db6859f0d47d0f4abf1c3cb7c756b76e429d
Nurono <while0pass@yandex.ru>
2015-01-14 23:09:32 +0300

    Полный спектр типов субстантивов по родам и числам.

--- slavdict/dictionary/models.py
+++ slavdict/dictionary/models.py
@@ -264,10 +264,18 @@ LANGUAGE_TRANSLIT_CSS = {
 SUBSTANTIVUS_TYPE_CHOICES = (
     ('a', u'ср.ед.'),
     ('b', u'ср.мн.'),
+    ('c', u'м.ед.'),
+    ('d', u'м.мн.'),
+    ('e', u'ж.ед.'),
+    ('f', u'ж.мн.'),
 )
 SUBSTANTIVUS_TYPE_MAP = {
     'n.sg.': 'a',
     'n.pl.': 'b',
+    'm.sg.': 'c',
+    'm.pl.': 'd',
+    'f.sg.': 'e',
+    'f.pl.': 'f',
 }
 
 
