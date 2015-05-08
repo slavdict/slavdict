161250b9ad42cf410729776fb7a19a7ebadddd94
Nurono <while0pass@yandex.ru>
2015-05-08 16:04:44 +0300

    При расстановке ссылок "ср." возможность удалить или поменять
    поле ``additional_info``.

--- bin/cf.py
+++ bin/cf.py
@@ -9,12 +9,25 @@ def cf(civil_equivalents):
     print 'Found %d entries for %d civil equivalents' % (
            len(entries), len(civil_equivalents))
     print 'Entries:', [e.pk for e in entries]
-    for e in entries:
-        print '%d --> %r' % (e.pk, [e.pk for e in e.cf_entries.all()])
-        print '    %s' % e.additional_info
+    for entry in entries:
+        print '%d %s --> %r\n    %s' % (
+                entry.pk, entry.civil_equivalent,
+                [e.pk for e in entry.cf_entries.all()],
+                entry.additional_info)
     with transaction.atomic():
         for entry in entries:
             entry.cf_entries = [e for e in entries if e.pk != entry.pk]
+            print '%d %s\n    %s' % (
+                    entry.pk, entry.civil_equivalent, entry.additional_info)
+            x = raw_input('\nDelete [d], change [c] or do nothing [N]\n'
+                          'with ``additional_info``: ').lower() or 'n'
+            if x == 'd':
+                entry.additional_info = u''
+                entry.save()
+            elif x == 'c':
+                x = raw_input('Input new ``additional_info``: ').decode('utf-8')
+                entry.additional_info = x
+                entry.save()
     return list(entries)
 
 # vi: set ai et sw=4 ts=4 :
