d366b10ec1b7cf2786e22cd81bf64127af93ce32
Nurono <while0pass@yandex.ru>
2016-08-04 13:55:57 +0300

    bugfix: не работало определение версии миграции базы данных

--- bin/dump_dictionary.sh
+++ bin/dump_dictionary.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-MIGRATION_VERSION=$(basename $(ls ../slavdict/dictionary/migrations/????_*.py | tail -1) | cut -d_ -f1)
+PRJDIR=$(dirname "$(dirname "$(readlink -e "$0")")")
+MIGRATION_VERSION=$(basename "$(ls "$PRJDIR"/slavdict/dictionary/migrations/????_*.py | tail -1)" | cut -d_ -f1)
 DBS_VERSION=4.$MIGRATION_VERSION
 GREP_SIGNATURE=::::
 NOW=$(date +"%Y.%m.%d--%H.%M")
-PRJDIR=$(dirname "$(dirname "$(readlink -e "$0")")")
 DUMPDIR="${1:-$PRJDIR/.dumps}"
-LASTFILE=$(ls -tA $DUMPDIR/.dictionary*.xml | head -1)
+LASTFILE=$(ls -tA "$DUMPDIR"/.dictionary*.xml | head -1)
 FILE="$DUMPDIR/.dictionary--$NOW---$DBS_VERSION.xml"
 
 python $PRJDIR/manage.py dumpdata dictionary --all --format=xml --indent=4 > $FILE
