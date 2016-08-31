0e897c2010b0043fdd13ed052e2a82994e235d6a
Nurono <while0pass@yandex.ru>
2016-08-31 18:44:30 +0300

    Выгрузка всех сокращений и слов используемых в адресах примеров

---
+++ bin/csv_address_marks.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+import re
+import sys
+
+import django
+sys.path.append(
+    os.path.dirname(os.path.dirname(
+            os.path.abspath(__file__))))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slavdict.settings')
+django.setup()
+
+from slavdict.dictionary.models import Example
+from slavdict.unicode_csv import UnicodeWriter
+
+def write_csv(filename, examples):
+    uw = UnicodeWriter(open(filename, 'w'))
+    NON_MARK_CHARS = ur'[\s\ \u00A0,0-9\.;:\-\u2011\!\(\)\[\]\?—–«»…]+'
+    register = {}
+    for e in (e for e in examples if e.host_entry.first_volume):
+        for mark in re.split(NON_MARK_CHARS, e.address_text):
+            if mark in register:
+                register[mark] = (register[mark][0] + 1, e)
+            else:
+                register[mark] = (1, e)
+    for mark, (number, e) in sorted(register.items()):
+        row = (
+            mark,
+            str(number),
+            e.address_text,
+            str(e.id),
+            e.host_entry.civil_equivalent,
+        )
+        uw.writerow(row)
+    uw.stream.close()
+
+write_csv('address_marks.csv', Example.objects.all())
