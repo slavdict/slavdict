3eaa3a9931683cadaa5f319f11991be99385c2a1
Nurono <while0pass@yandex.ru>
2018-01-07 04:58:52 +0300

    Джанговская админка должна использовать не jinja2, а стандартный
    джанговский шаблонизатор

--- slavdict/settings.py
+++ slavdict/settings.py
@@ -104,6 +104,19 @@ WSGI_APPLICATION = 'slavdict.wsgi.application'
 
 TEMPLATES = [
     {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+    {
         'BACKEND': 'django.template.backends.jinja2.Jinja2',
         'DIRS': [
             ROOT + 'templates/',
