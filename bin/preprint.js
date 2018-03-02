5a68a85c51990a5c76701067866af06882551c61
Nurono <while0pass@yandex.ru>
2018-03-02 13:37:45 +0300

    Скрипт на javascript по настройке стилей страницы перед экспортом
    её в pdf обновлен в соответствии с новыми шаблонами отображения.

--- bin/preprint.js
+++ bin/preprint.js
@@ -1,10 +1,14 @@
-$('.entry').css('margin-top', '40px');
+$('#main, #main > div')
+    .css('witdht', '32em')
+    .css('padding', 0)
+    .css('margin', 0);
 
-$('.entry-collogroups')
-	.css('background-color', 'white')
-	.css('margin','0 0 0 -11px');
+$('.Entry').css('line-height', '1.8em');
+$('.FirstParagraph').css('text-indent', 0);
 
 $('*').each(function(){
 	var x = $(this);
 	x.css('font-family', x.css('font-family'));
 });
+
+$('.author').remove();
