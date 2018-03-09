a3c46367620e2ebc7fb87bd021ebbd21717f0efb
Nurono <while0pass@yandex.ru>
2018-03-09 18:50:52 +0300

    Добавлена ссылка для разлогинивания

--- slavdict/urls.py
+++ slavdict/urls.py
@@ -9,14 +9,13 @@ from django.views.generic import TemplateView
 from slavdict.admin import ui
 admin.autodiscover()
 
-LOGIN_URL = 'login/'
 urlpatterns = [
     url( r'^',           include('slavdict.dictionary.urls') ),
     url( r'^admin/',     include(admin.site.urls) ),
     url( r'^admin/doc/', include('django.contrib.admindocs.urls') ),
     url( r'^ui/',        include(ui.urls) ),
-    url( r'^%s$' % LOGIN_URL,    LoginView.as_view() ),
-    url( r'^logout/$', LogoutView.as_view(next_page=LOGIN_URL), name='logout' ),
+    url( r'^login/$',    LoginView.as_view() ),
+    url( r'^logout/$',   LogoutView.as_view(next_page='/'), name='logout' ),
     url( r'^test/$',     TemplateView.as_view(template_name='test.html') ),
 ]
 
