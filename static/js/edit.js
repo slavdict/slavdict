61af3aab1c39f9272486de2b7e747cee407f5d7d
Nurono <while0pass@yandex.ru>
2018-03-23 12:06:50 +0300

    Ф-ция getCookie вынесена в отдельный файл

--- static/js/edit.js
+++ static/js/edit.js
@@ -1,24 +1,6 @@
 try {
 
 
-
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = $.trim(cookies[i]);
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-var csrftoken = getCookie('csrftoken');
-
-
 var topic = 'entry_change',
     otStyle = 'slavdictOpentip',
     constructors = [Etymology, Participle, Orthvar,
