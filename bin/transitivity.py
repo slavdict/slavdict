dc1b051ab6b7a7ebd9801583819aea010e48975f
Nurono <while0pass@yandex.ru>
2018-08-29 13:33:36 +0300

    Поиск словоформ в примерах на основе метрики, использующей
    измененное расстояние Левенштейна. Дополонительный вес дается тем
    словоформам, которые начинаются с той же буквы, что лексема. Но
    поискольку преобразование в гражданку иногда идет с потерями и некоторые
    буквы заменяются на звездочки, метрика невсегда дает правильные
    результаты.

--- bin/transitivity.py
+++ bin/transitivity.py
@@ -3,6 +3,23 @@ from itertools import chain
 
 from slavdict.dictionary.models import *
 
+def distance(a, b):
+    "Calculates the Levenshtein distance between a and b."
+    n, m = len(a), len(b)
+    if n > m:
+        # Make sure n <= m, to use O(min(n, m)) space
+        a, b = b, a
+        n, m = m, n
+    cur_row = range(n+1)  # Keep current and previous row, not entire matrix
+    for i in range(1, m+1):
+        pre_row, cur_row = cur_row, [i]+[0]*n
+        for j in range(1,n+1):
+            add, delete, change = pre_row[j]+1, cur_row[j-1]+1, pre_row[j-1]
+            if a[j-1] != b[i-1]:
+                change += 1
+            cur_row[j] = min(add, delete, change)
+    return cur_row[n]
+
 def transitivity(arg):
     if isinstance(arg, int):
         e = Entry.objects.get(pk=arg)
@@ -28,9 +45,12 @@ def transitivity(arg):
         for ex in chain(meaning.examples,
                 *(m.examples for m in meaning.child_meanings)):
             example = civilrus_convert(ex.example)
-            words = [u'\033[1;33m%s\033[0m' % w
-                     if w.lower()[:4] == e.civil_equivalent[:4]
-                     else w for w in example.split()]
+            words_lds = [(w, distance(e.civil_equivalent, w.lower()) -
+                             w.lower().startswith(e.civil_equivalent[:1]) * 2)
+                         for w in example.split()]
+            ld_min = min(ld for w, ld in words_lds)
+            words = [u'\033[1;33m%s\033[0m' % w if ld == ld_min else w
+                     for w, ld in words_lds]
             print u'\t\t*  %s' % u' '.join(words)
 
         while True:
