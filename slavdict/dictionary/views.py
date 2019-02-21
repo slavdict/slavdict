ba4bd2611ea72945dceb7f1a2ec8e17d8772a60e
Nurono <while0pass@yandex.ru>
2019-02-21 15:23:19 +0300

    Ускоритель для поиска вопросительных знаков в значениях

--- slavdict/dictionary/views.py
+++ slavdict/dictionary/views.py
@@ -1223,6 +1223,14 @@ def useful_urls_redirect(uri, request):
         uri = uri_qs(mURI, id__in=','.join(str(m.id) for m in ms),
                      volume=VOLUME)
 
+    elif uri == 'meanings_quest':
+        regex = re.compile(ur'[?!]',
+                           flags=re.MULTILINE | re.IGNORECASE | re.UNICODE)
+        ms = (m for m in Meaning.objects.all()
+                if m.not_hidden() and regex.search(m.meaning + m.gloss))
+        uri = uri_qs(mURI, id__in=','.join(str(m.id) for m in ms),
+                     volume=VOLUME)
+
     elif uri == 'headwords_titles':
         es = []
         r = re.compile(ur'[~АБВГДЕЄЖЗЅИЙІКЛМНОѺПРСТѸУФХѾЦЧШЩѢЫЮꙖѠѼѦѮѰѲѴ]')
@@ -1448,6 +1456,7 @@ def useful_urls(request, x=None, y=None):
                     (u'С пометой "в роли .."', 'meanings_ps'),
                     (u'С текстом "в знач...."', 'meanings_ps2'),
                     (u'С текстом "с прям. речью"', 'meanings_direct_speech'),
+                    (u'С текстом "[?!]"', 'meanings_quest'),
                 )),
     )
     if x:
