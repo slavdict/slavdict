94d5032ad409cd7aa682e288438b8a8ce0a5f71c
Nurono <while0pass@yandex.ru>
2019-04-16 20:16:02 +0300

    Замена заглавных букв в начале слов в примерах на строчные

---
+++ bin/lower_capitals.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+import re
+
+from slavdict.dictionary.models import Example
+
+# Примеры, где сохранение заглавных букв принципиально важно.
+# Все они в статьях, посвященных отдельным буквам.
+exclude_ids = (
+    8196, 8197, 8236, 8202,  # А
+    8226, 8235, 8227,  # Б
+    8229, 8230, 8237, 8231,  # В
+    23812, 23813, 23814, 23819,  # Г
+    25130, 25128, 25127, 25137,  # Д
+    27683,  # Ять
+)
+
+r = (
+    ur'(?:^|(?<=[^'
+    ur'абвгдеєжѕзийіıклмноѻпрстѹꙋуфхѿцчшщъыьѣюꙗѡѽѧѯѱѳѵ'
+    ur'АБВГДЕЄЖЗЅИЙІКЛМНОѺПРСТѸУꙊФХѾЦЧШЩЪЫЬѢЮꙖѠѼѦѮѰѲѴ~'
+    ur"\^'`"
+    ur']))'
+    ur'([АБВГДЕЄЖЗЅИЙІКЛМНОѺПРСТѸУФХѾЦЧШЩЪЫЬѢЮꙖѠѼѦѮѰѲѴꙊ])'
+)
+
+for ex in Example.objects.exclude(id__in=exclude_ids):
+    segments = re.split(r, ex.example)
+    if len(segments) > 1:
+        print
+        print ex.example
+        ex.example = u''.join(s if i % 2 == 0 else s.lower()
+                              for i, s in enumerate(segments))
+        print ex.example
+        ex.save()
