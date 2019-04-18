5eeb982ae2fbb41d8e5921d9d98da03c5f9eb77c
Nurono <while0pass@yandex.ru>
2019-04-18 12:32:24 +0300

    При сортировке адресов исключать всю пунктуацию и различия в регистре

--- bin/sort_example_addresses.py
+++ bin/sort_example_addresses.py
@@ -3,7 +3,9 @@ import sys
 
 def sort_key(line):
     fragment = line.split(u'\t')[-1]
-    return re.sub(ur'[\s\u00a0\u202f\u2060\u200b]', u'', fragment)
+    fragment = re.sub(ur'[\s\u00a0\u202f\u2060\u200b]', u'', fragment)
+    fragment = re.sub(ur'[\.,:;\-—–‑"«»]', u'', fragment)
+    return (fragment.lower(), fragment)
 
 lines = sys.stdin.read().decode('utf-8').split('\n')
 lines.sort(key=sort_key)
