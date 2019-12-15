9695a39e45a9b15eb4c11711a9dec8d22552fa43
Nurono <while0pass@yandex.ru>
2019-12-15 18:22:58 +0300

    Донастройки WSGI

--- slavdict/wsgi.py
+++ slavdict/wsgi.py
@@ -1,10 +1,2 @@
-import sys
-PATH = '/var/www/slavdict'
-if PATH not in sys.path:
-    sys.path.append(PATH)
-
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slavdict.settings')
-
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
