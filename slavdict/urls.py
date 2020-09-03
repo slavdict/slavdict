4a590fe39847677e2960c2780b51d36d8c072856
Nurono <while0pass@yandex.ru>
2020-09-03 16:20:24 +0300

    После успешной смены пароля редирект на главную страницу

--- slavdict/urls.py
+++ slavdict/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import PasswordChangeView
 from django.views.generic import TemplateView
 
 from slavdict.dictionary.admin import ui
@@ -9,6 +10,8 @@ admin.autodiscover()
 
 urlpatterns = [
     url( r'^',           include('slavdict.dictionary.urls') ),
+    url( r'^admin/password_change/', PasswordChangeView.as_view(success_url='/'),
+                                     name='password_change' ),
     url( r'^admin/',     admin.site.urls ),
     url( r'^admin/doc/', include('django.contrib.admindocs.urls') ),
     url( r'^ui/',        ui.urls ),
