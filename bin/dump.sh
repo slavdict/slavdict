a6714ed6f4af7632f9c10fa3fd0a24fe30ed499d
Nurono <while0pass@yandex.ru>
2020-11-19 12:58:22 +0300

    Перечислять файлы последних трех месяцев для фильтров rsync'а

--- bin/dump.sh
+++ bin/dump.sh
@@ -5,3 +5,11 @@ VERBOSITY=${2:-0}
 $PRJDIR/bin/dump_csl.sh $DUMPDIR $VERBOSITY
 $PRJDIR/bin/dump_users.sh $DUMPDIR
 $PRJDIR/bin/dump_dictionary.sh $DUMPDIR $VERBOSITY
+find $DUMPDIR \
+  -mindepth 1 \
+  -and -mtime -93 \
+  -and ! -name .rsync-filter \
+  -and ! -name 'dictionary*.xml' \
+  | xargs basename -a
+  | sed -e 's/^/+ /; $s/$/\n- */' \
+  > $DUMPDIR/.rsync-filter
